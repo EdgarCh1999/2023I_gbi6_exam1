dc9fd4b59e98a43629f8c9428b971b3635e10fd0
EdgarCh1999 <131310370+EdgarCh1999@users.noreply.github.com>
2023-07-01 17:22:59 -0500

    Edgar Chavez ha realizado el ejericio 1.1

--- exam2_Python/popsimulate.py
+++ exam2_Python/popsimulate.py
@@ -1,15 +1,25 @@
 #_________________ Función 1
 import numpy
 def build_population(N, p):
+# Crea una lista vacía para almacenar la población
     population = []
+# Repite N veces para crear N individuos
     for i in range(N):
+    # Establece el primer alelo en "A"
         allele1 = "A"
+        # Genera un número aleatorio y verifica si es mayor que p
         if numpy.random.rand() > p:
+             # Si es mayor que p, cambia el primer alelo a "a"
             allele1 = "a"
+            # Establece el segundo alelo en "A"
         allele2 = "A"
+        # Genera otro número aleatorio y verifica si es mayor que p
         if numpy.random.rand() > p:
+            # Si es mayor que p, cambia el segundo alelo a "a"
             allele2 = "a"
+         # Agrega la tupla de alelos a la lista de población
         population.append((allele1, allele2))
+     # Devuelve la lista de población
     return population
 
 #_________________ Función 2
